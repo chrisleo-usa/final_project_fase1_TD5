8a47d5251a476419c9c67bb50d7bd55275397bd6
christopher alves <chrisleo.usa@gmail.com>
2021-02-27 23:28:11 -0300

    refatorado candidate update profile teste

--- spec/features/candidate/candidate_update_profile_spec.rb
+++ spec/features/candidate/candidate_update_profile_spec.rb
@@ -1,34 +1,29 @@
 require 'rails_helper'
 
 feature 'Candidate can update his own profile' do
-  scenario 'must be signed in' do
+  scenario 'and can access his informations from root_path' do
     candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
                                   biography: 'Profissional da área de eventos migrando para a área da tecnologia',
                                   email: 'chris@campuscode.com', password: '123456')
-
-    visit root_path
-    click_on 'Login'
-    click_on 'Candidate'
-    within('form') do
-      fill_in 'Email', with: candidate.email
-      fill_in 'Password', with: candidate.password
-      click_on 'Log in'
-    end
-
-    expect(current_path).to eq(candidate_path(candidate))
-    expect(page).to have_content(candidate.name)
-  end
-
-  scenario 'and can access his informations from root_path' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-      biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-      email: 'chris@campuscode.com', password: '123456')
 
     login_as candidate, scope: :candidate
     visit root_path
     click_on 'My profile'
 
     expect(current_path).to eq(candidate_path(candidate))
+  end
+
+  scenario 'see edit button' do
+    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
+                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
+                                  email: 'chris@campuscode.com', password: '123456')
+
+    login_as candidate, scope: :candidate
+    visit root_path
+    click_on 'My profile'
+
+    expect(current_path).to eq(candidate_path(candidate))
+    expect(page).to have_link('Edit')
   end
 
   scenario 'attributes cannot be blank' do
@@ -39,7 +34,7 @@ feature 'Candidate can update his own profile' do
     login_as candidate, scope: :candidate
     visit candidate_path(candidate)
     click_on 'Edit'
-    within('form.signup__form') do
+    within('form.create__form') do
       fill_in 'Full name', with: ''
       fill_in 'Phone', with: ''
       fill_in 'CPF', with: ''
@@ -66,7 +61,7 @@ feature 'Candidate can update his own profile' do
     login_as candidate, scope: :candidate
     visit candidate_path(candidate)
     click_on 'Edit'
-    within('form.signup__form') do
+    within('form.create__form') do
       fill_in 'Full name', with: 'Susan Ristau'
       fill_in 'Phone', with: '4891122334455'
       fill_in 'CPF', with: '98765432109'
@@ -77,16 +72,16 @@ feature 'Candidate can update his own profile' do
     end
 
     expect(current_path).to eq(candidate_path(candidate))
-    expect(page).not_to have_content('Christopher Alves')
-    expect(page).not_to have_content('48988776655')
-    expect(page).not_to have_content('12345678910')
-    expect(page).not_to have_content('Profissional da área de eventos migrando para a área da tecnologia')
-    expect(page).not_to have_content('chris@campuscode.com')
+    expect(page).not_to have_css('h2.dashboard__name', text: "#{candidate.name} profile")
+    expect(page).not_to have_css('p.dashboard__attribute', text: '48988776655')
+    expect(page).not_to have_css('p.dashboard__attribute', text: '12345678910')
+    expect(page).not_to have_css('p.dashboard__attribute', text: 'Profissional da área de eventos migrando para a área da tecnologia')
+    expect(page).not_to have_css('p.dashboard__attribute', text: 'chris@campuscode.com')
 
-    expect(page).to have_content('Susan Ristau')
-    expect(page).to have_content('4891122334455')
-    expect(page).to have_content('98765432109')
-    expect(page).to have_content('Promotora de eventos e cerimonialista de casamentos')
-    expect(page).to have_content('susan@ristau.com')
+    expect(page).to have_css('h2.dashboard__name', text: 'Susan Ristau')
+    expect(page).to have_css('p.dashboard__attribute', text: '4891122334455')
+    expect(page).to have_css('p.dashboard__attribute', text: '98765432109')
+    expect(page).to have_css('p.dashboard__attribute', text: 'Promotora de eventos e cerimonialista de casamentos')
+    expect(page).to have_css('p.dashboard__attribute', text: 'susan@ristau.com')
   end
 end
