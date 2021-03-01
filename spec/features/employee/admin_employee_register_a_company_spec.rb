418a3026b9566187f23f16e601890fa2cbd4653f
christopher alves <chrisleo.usa@gmail.com>
2021-03-01 18:16:10 -0300

    Refatorado admin employee register company

--- spec/features/employee/admin_employee_register_a_company_spec.rb
+++ spec/features/employee/admin_employee_register_a_company_spec.rb
@@ -1,29 +1,34 @@
 require 'rails_helper'
 
-#TODO: No edit ao dar refresh ele libera cadastrar a empresa sem os dados. Corrigir essa situação
 feature 'The first employee to register need register the company as well' do
-  scenario 'only the first employee can register the company' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', domain: 'campuscode')
-    Employee.create!(email: 'chris@campuscode.com', password: '123456', admin: 0, company: company)
+  scenario 'only the first employee is redirect to register company' do
+    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
+                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
+                              domain: 'campuscode')
+    admin_employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', admin: 1, company: company)
 
     visit root_path
     click_on 'Login'
     click_on 'Employee'
     click_on 'Sign up'
-    within('form') do
+    within 'form.create__form' do
       fill_in 'Email', with: 'joao@campuscode.com.br'
       fill_in 'Password', with: '123456'
       fill_in 'Password confirmation', with: '123456'
       click_on 'Sign up'
     end
 
-    expect(current_path).to eq(company_path(Employee.last.company))
-    expect(page).to have_content(Employee.last.email)
+    employee = Employee.last
+    expect(current_path).not_to eq(edit_company_path(company))
+    expect(current_path).to eq(company_path(employee.company))
+    expect(page).to have_content(employee.email)
+    expect(page).to have_css('h2.dashboard__name', text: "#{company.name} profile")
     expect(page).to have_link('Logout')
+    expect(page).not_to have_content(admin_employee.email)
     expect(page).not_to have_link('Register a job opportunity')
   end
 
-  scenario 'view company register form' do
+  scenario 'and view company register form' do
     company = Company.create!(name: 'Company name', address: 'Company address', site: 'Company site', domain: 'campuscode')
     employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', admin: 1, company: company)
 
@@ -32,7 +37,7 @@ feature 'The first employee to register need register the company as well' do
 
     expect(current_path).to eq (edit_company_path(Employee.last.company))
     expect(page).to have_content(employee.email)
-    within('form') do
+    within('form.create__form') do
       expect(page).to have_content('Company name')
       expect(page).to have_content('Logo')
       expect(page).to have_content('Address')
@@ -43,32 +48,35 @@ feature 'The first employee to register need register the company as well' do
     end
   end
 
-  scenario 'attributes name, address and site cannot be blank' do
+  scenario 'attributes name, address and site cannot be blank on edit' do
     company = Company.create!(name: 'Company name', address: 'Company address', site: 'Company site', domain: 'campuscode')
     employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', admin: 1, company: company)
 
     login_as employee, scope: :employee
     visit edit_company_path(employee.company)
-    within 'form' do
+    within 'form.create__form' do
       fill_in 'Company name', with: ''
       fill_in 'Address', with: ''
       fill_in 'Site', with: ''
       click_on 'Save'
     end
 
-    expect(current_path).to eq (company_path(Employee.last.company))
-    expect(page).to have_content('Name can\'t be blank')
-    expect(page).to have_content('Address can\'t be blank')
-    expect(page).to have_content('Site can\'t be blank')
+    employee = Employee.last
+    expect(current_path).to eq (company_path(employee.company))
+    within('div.warnings') do
+      expect(page).to have_content('Name can\'t be blank')
+      expect(page).to have_content('Address can\'t be blank')
+      expect(page).to have_content('Site can\'t be blank')
+    end
   end
 
-  scenario 'successfully register his company' do
-    company = Company.create!(name: 'Company name', address: 'Company address', site: 'Company site', domain: 'campuscode')
+  scenario 'successfully register company' do
+    company = Company.create!(domain: 'campuscode')
     employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company, admin: 1)
 
     login_as employee, scope: :employee
     visit edit_company_path(employee.company)
-    within 'form' do
+    within 'form.create__form' do
       fill_in 'Company name', with: 'Campus Code'
       attach_file 'Logo', Rails.root.join('spec', 'support', 'tdlogo.png')
       fill_in 'Address', with: 'rua são paulo'
@@ -78,12 +86,15 @@ feature 'The first employee to register need register the company as well' do
       click_on 'Save'
     end
 
+    company = Company.last
     expect(current_path).to eq(company_path(Company.last))
-    expect(page).to have_content('Campus Code')
-    expect(page).to have_content('rua são paulo')
-    expect(page).to have_content('12345678910112')
-    expect(page).to have_content('www.campuscode.com.br')
-    expect(page).to have_content('www.instagram.com/treinadev')
-    expect(page).to have_css('img[src*="tdlogo.png"]')
+    within('div.dashboard__grid') do
+      expect(page).to have_css('p.dashboard__attribute', text: 'Campus Code')
+      expect(page).to have_css('p.dashboard__attribute', text: 'rua são paulo')
+      expect(page).to have_css('p.dashboard__attribute', text: '12345678910112')
+      expect(page).to have_css('p.dashboard__attribute', text: 'www.campuscode.com.br')
+      expect(page).to have_css('p.dashboard__attribute', text: 'www.instagram.com/treinadev')
+      expect(page).to have_css('img[src*="tdlogo.png"]')
+    end
   end
 end
