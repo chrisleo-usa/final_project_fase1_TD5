e55ba84e3956c2aa2a3fa11b33eb3d5338a0aa8f
christopher alves <chrisleo.usa@gmail.com>
2021-03-12 08:02:47 -0300

    refatorado teste desaprova inscrição com factory bot

---
+++ spec/factories/rejects.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :reject do
+    message { 'Agradecemos o interesse, mas procuramos pessoas mais experientes.' }
+  end
+end
--- spec/features/employee/employee_disapprove_a_enrollment_spec.rb
+++ spec/features/employee/employee_disapprove_a_enrollment_spec.rb
@@ -2,19 +2,10 @@ require 'rails_helper'
 
 feature 'A employee disapprove a enrollment' do
   scenario 'can see deny button' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011,
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode',
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company)
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@gmail.com', password: '123456')
-
+    company = create(:company)
+    employee = create(:employee, company: company)
+    job = create(:job, title: 'Ruby on Rails Developer', company: company)
+    candidate = create(:candidate, name: 'Christopher Alves')
     enrollment = Enrollment.create!(job: job, candidate: candidate)
 
     login_as employee, scope: :employee
@@ -28,21 +19,12 @@ feature 'A employee disapprove a enrollment' do
   end
 
   scenario 'but cannot see the deny button if enrollment is already disapproved' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011,
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode',
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company)
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@gmail.com', password: '123456')
-
+    company = create(:company)
+    employee = create(:employee, company: company)
+    job = create(:job, company: company)
+    candidate = create(:candidate)
     enrollment = Enrollment.create!(job: job, candidate: candidate, status: :denied)
-    Reject.create!(message: 'Agradecemos o interesse, mas procuramos pessoas mais experientes.', enrollment: enrollment)
+    create(:reject, enrollment: enrollment)
 
     login_as employee, scope: :employee
     visit enrollment_path(enrollment)
@@ -52,19 +34,10 @@ feature 'A employee disapprove a enrollment' do
   end
 
   scenario 'see form to fill with disapproval message' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011,
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode',
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company)
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@gmail.com', password: '123456')
-
+    company = create(:company)
+    employee = create(:employee, company: company)
+    job = create(:job, company: company)
+    candidate = create(:candidate)
     enrollment = Enrollment.create!(job: job, candidate: candidate)
 
     login_as employee, scope: :employee
@@ -76,19 +49,10 @@ feature 'A employee disapprove a enrollment' do
   end
 
   scenario 'successfully' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011,
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode',
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company)
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@gmail.com', password: '123456')
-
+    company = create(:company)
+    employee = create(:employee, company: company)
+    job = create(:job, company: company)
+    candidate = create(:candidate)
     enrollment = Enrollment.create!(job: job, candidate: candidate)
 
     login_as employee, scope: :employee
