dfaf6a7dcbaf716aad7aa40e58d9568fa4d4d1db
christopher alves <chrisleo.usa@gmail.com>
2021-03-12 11:33:30 -0300

    refatorado testes de candidato aceita proposa com factory bot

--- spec/features/candidate/candidate_accept_proposal_spec.rb
+++ spec/features/candidate/candidate_accept_proposal_spec.rb
@@ -2,21 +2,11 @@ require 'rails_helper'
 
 feature 'Candidate accept proposal' do
   scenario 'attribute cannot be blank' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910, 
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia', 
-                                  email: 'chris@campuscode.com', password: '123456')
-
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
+    candidate = create(:candidate)
+    company = create(:company)
+    job = create(:job, company: company)
     enrollment = Enrollment.create!(job: job, candidate: candidate, status: :approved)
-    proposal = Proposal.create!(proposal_message: 'Congratulations, you have been approved', 
-                                proposal_salary: 5000.0, start_date: '10/05/2023', enrollment: enrollment)
+    proposal = create(:proposal, enrollment: enrollment)
 
     login_as candidate, scope: :candidate
     visit enrollment_path(enrollment)
@@ -35,21 +25,11 @@ feature 'Candidate accept proposal' do
   end
 
   scenario 'sucessfully' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910, 
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia', 
-                                  email: 'chris@campuscode.com', password: '123456')
-
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
+    candidate = create(:candidate)
+    company = create(:company)
+    job = create(:job, company: company)
     enrollment = Enrollment.create!(job: job, candidate: candidate, status: :approved)
-    proposal = Proposal.create!(proposal_message: 'Congratulations, you have been approved', 
-                                proposal_salary: 5000.0, start_date: '10/05/2023', enrollment: enrollment)
+    proposal = create(:proposal, enrollment: enrollment)
 
     login_as candidate, scope: :candidate
     visit enrollment_path(enrollment)
@@ -62,8 +42,8 @@ feature 'Candidate accept proposal' do
 
     expect(current_path).to eq(proposal_path(proposal))
     expect(page).to have_css('span.accepted', text: 'Aceita')
-    expect(page).not_to have_css('span.dashboard__status', text: 'Em análise')
-    expect(page).not_to have_css('span.dashboard__status', text: 'Recusada')
+    expect(page).not_to have_css('span.pending', text: 'Em análise')
+    expect(page).not_to have_css('span.declined', text: 'Recusada')
     expect(page).not_to have_link('Recusar')
     expect(page).not_to have_link('Aceitar')
     expect(page).to have_content('Parabéns, bem vindo a sua nova jornada!')
