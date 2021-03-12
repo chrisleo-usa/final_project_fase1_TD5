c8b360f1d2e2a44c3c7eac80420c7ffbbe74f958
christopher alves <chrisleo.usa@gmail.com>
2021-03-12 11:01:29 -0300

    refatorado testes de atualizar empresa com factory bot

--- spec/features/employee/employee_update_company_spec.rb
+++ spec/features/employee/employee_update_company_spec.rb
@@ -1,65 +1,60 @@
 require 'rails_helper'
 
 feature 'Admin employee update company info' do
-  scenario 'and must be signed in' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company)
+  scenario 'but must be signed in to see edit button' do
+    company = create(:company, name: 'Campus Code')
 
-    login_as employee, scope: :employee
     visit root_path
+    click_on 'Empresas'
+    click_on company.name
 
-    expect(page).not_to have_link('Login')
-    expect(page).to have_content(employee.email)
-    expect(page).to have_link('Logout')
+    expect(current_path).to eq(company_path(company))
+    expect(page).not_to have_link('Editar')
   end
 
   scenario 'regular employee cannot see edit button' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company, admin: 0)
+    company = create(:company)
+    employee = create(:employee, company: company, admin: 0)
 
     login_as employee, scope: :employee
     visit root_path
     click_on 'Minha empresa'
 
     expect(current_path).to eq(company_path(company))
-    expect(page).not_to have_link('Edit.create__btn')
+    expect(page).not_to have_link('Editar')
   end
 
   scenario 'and attributes cannot be blank' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company, admin: 1)
+    company = create(:company)
+    employee = create(:employee, company: company, admin: 1)
 
     login_as employee, scope: :employee
     visit company_path(company)
     click_on 'Editar'
     within 'form.create__form' do
       fill_in 'Nome da empresa', with: ''
-      attach_file 'Logo', Rails.root.join('spec', 'support', 'ruby.jpeg')
       fill_in 'Endereço', with: ''
       fill_in 'Complemento', with: ''
       fill_in 'CNPJ', with: ''
       fill_in 'Site', with: ''
-      fill_in 'Rede social', with: 'www.linkedin.com/in/campuscode'
+      fill_in 'Rede social', with: ''
       click_on 'Salvar'
     end
 
     expect(current_path).to eq(company_path(company))
-    expect(page).to have_content('Nome da empresa não pode ficar em branco')
-    expect(page).to have_content('Endereço não pode ficar em branco')
-    expect(page).to have_content('Site não pode ficar em branco')
+    within('div.warnings') do
+      expect(page).to have_content('Nome da empresa não pode ficar em branco')
+      expect(page).to have_content('Endereço não pode ficar em branco')
+      expect(page).to have_content('Complemento não pode ficar em branco')
+      expect(page).to have_content('CNPJ não pode ficar em branco')
+      expect(page).to have_content('Site não pode ficar em branco')
+      expect(page).to have_content('Rede social não pode ficar em branco')
+    end
   end
 
   scenario 'successfully' do
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-    employee = Employee.create!(email: 'chris@campuscode.com', password: '123456', company: company, admin: 1)
+    company = create(:company, name: 'Campus Code')
+    employee = create(:employee, company: company, admin: 1)
 
     login_as employee, scope: :employee
     visit edit_company_path(company)
@@ -68,8 +63,8 @@ feature 'Admin employee update company info' do
       attach_file 'Logo', Rails.root.join('spec', 'support', 'ruby.jpeg')
       fill_in 'Endereço', with: 'Rua do TreinaDev'
       fill_in 'Complemento', with: '225, sala 01'
-      select 'São Paulo', from: 'Cidade'
-      select 'SP', from: 'Estado'
+      select 'Florianópolis', from: 'Cidade'
+      select 'SC', from: 'Estado'
       fill_in 'CNPJ', with: '987654321014'
       fill_in 'Site', with: 'www.treinadev.com.br'
       fill_in 'Rede social', with: 'www.linkedin.com/in/treinadev'
@@ -80,12 +75,12 @@ feature 'Admin employee update company info' do
     expect(page).to have_css('p.dashboard__attribute', text: 'TreinaDev')
     expect(page).to have_css('p.dashboard__attribute', text: 'Rua do TreinaDev')
     expect(page).to have_css('p.dashboard__attribute', text: '225, sala 01')
-    expect(page).to have_css('p.dashboard__attribute', text: 'São Paulo')
-    expect(page).to have_css('p.dashboard__attribute', text: 'SP')
+    expect(page).to have_css('p.dashboard__attribute', text: 'Florianópolis')
+    expect(page).to have_css('p.dashboard__attribute', text: 'SC')
     expect(page).to have_css('p.dashboard__attribute', text: '987654321014')
     expect(page).to have_css('p.dashboard__attribute', text: 'www.treinadev.com.br')
     expect(page).to have_css('p.dashboard__attribute', text: 'www.linkedin.com/in/treinadev')
     expect(page).to have_css('img[src*="ruby.jpeg"]')
-    expect(page).not_to have_content(company.name)
+    expect(page).not_to have_content('Campus Code')
   end
 end
