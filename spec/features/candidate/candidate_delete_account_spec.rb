f6cd02e627e41215a20da1b9c96d7e8e3b0fad43
christopher alves <chrisleo.usa@gmail.com>
2021-03-12 13:19:04 -0300

    refatorado testes de candidato deleta conta com factory bot

--- spec/features/candidate/candidate_delete_account_spec.rb
+++ spec/features/candidate/candidate_delete_account_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 feature 'Candidate delete account' do
   scenario 'see delete button' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@campuscode.com', password: '123456')
+    candidate =create(:candidate)
 
     login_as candidate, scope: :candidate
     visit candidate_path(candidate)
@@ -13,9 +11,7 @@ feature 'Candidate delete account' do
   end
 
   scenario 'successfully' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@campuscode.com', password: '123456')
+    candidate = create(:candidate)
 
     login_as candidate, scope: :candidate
     visit candidate_path(candidate)
@@ -28,18 +24,9 @@ feature 'Candidate delete account' do
   end
 
   scenario 'even if had a enrollment associated' do
-    candidate = Candidate.create!(name: 'Christopher Alves', phone: '48988776655', cpf: 12345678910,
-                                  biography: 'Profissional da área de eventos migrando para a área da tecnologia',
-                                  email: 'chris@campuscode.com', password: '123456')
-
-    company = Company.create!(name: 'Campus Code', address: 'Rua São Paulo, 222', cnpj: 1234567891011, 
-                              site: 'www.campuscode.com.br', social_media: 'www.linkedin.com/in/campuscode', 
-                              domain: 'campuscode')
-
-    job = Job.create!(title: 'Ruby on Rails Developer', description: 'Vaga para Ruby on Rails Developer', 
-                      salary_range: 9000.0, requirements: 'Conhecimento sólido em Java, Ruby, Ruby on Rails, NodeJS, SQLite3',
-                      deadline_application: '10/04/2023', total_vacancies: 2, level: 1, company: company)
-
+    candidate = create(:candidate)
+    company = create(:company)
+    job = create(:job, company: company)
     Enrollment.create!(job: job, candidate: candidate)
 
     login_as candidate, scope: :candidate
